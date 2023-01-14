790a088e121e8077bf37ca93e0bcbf77c9df94ab
javierarguello <javierarguello@users.noreply.github.com>
2023-01-14 10:29:24 +0000

    deploy: 3f3b6383c65dcf991ba7e8560f05325f8243881e

---
+++ _next/static/XcS-BUqT-H0LLSmWSGuev/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8546c03b6320ced5.css","static/chunks/pages/index-5b205b6595309632.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
