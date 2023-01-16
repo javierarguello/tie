98cd834b7bc7930daafc51b328580c79ace20481
Javier Arguello <javier@qurable.co>
2023-01-16 18:29:44 +0100

    page title

--- pages/offices.tsx
+++ pages/offices.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from 'next'
 import { IOffice } from "../models/office";
 import fsPromises from 'fs/promises'
 import path from 'path'
+import Head from "next/head";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const jsonData = await fsPromises.readFile(path.join(process.cwd(), "./data/offices.json"));
@@ -15,11 +16,22 @@ export const getStaticProps: GetStaticProps = async (context) => {
 }
 
 export default function Offices({ offices }: { offices: IOffice[] }) {
+  const pageTitle = "Consulta los últimos Lotes de TIE";
+  const description = "Últimos Lotes de TIE"
   return (
-    <div className="flex flex-col items-start w-full">
-      {offices.map(office =>
-        <OfficeInfo key={office.name} name={office.name} location={office.location} currentBatch={office.currentBatch} />
-      )}
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={""} key="description" />
+        <meta property="og:description" content={description} key="og:description" />
+        <meta property="og:title" content={pageTitle} key="og:title" />
+        <meta property="title" content={pageTitle} key="title" />
+      </Head>
+      <div className="flex flex-col items-start w-full">
+        {offices.map(office =>
+          <OfficeInfo key={office.name} name={office.name} location={office.location} currentBatch={office.currentBatch} />
+        )}
+      </div>
+    </>
   )
 }
